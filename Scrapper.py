1dbf5d2437a74ae973ccd5d6ebacfb72ece66dcc
SzymonFit <szymek.fit@gmail.com>
2023-07-16 12:25:39 +0200

    16.07, changes regarding switching to other sites

--- Scrapper.py
+++ Scrapper.py
@@ -1,47 +1,76 @@
 import requests
 from bs4 import BeautifulSoup
-from urllib.parse import urljoin
 import json
 
 class Scrapper:
     
     def __init__(self) -> None:
-        self.list_of_urls = ["https://www.pracuj.pl/praca/intern%20it;kw/krakow;wp?rd=0&et=1","https://nofluffjobs.com/pl/krakow?criteria=seniority%3Dtrainee&page=1"]
+        self.list_of_urls = ["https://www.pracuj.pl/praca/intern%20it;kw/krakow;wp?rd=0&et=1","https://nofluffjobs.com/pl/krakow?page=1"]
         self.main_dict = {'pracuj':None, 'nofluffjobs':None}
         self.noFluffJobs()
         self.pracuj()
         
     def noFluffJobs(self):
-        
-        response = requests.get(self.list_of_urls[1])
-        soup = BeautifulSoup(response.text, "html.parser")
-        h3_elements = soup.select('nfj-postings-list h3')
-        text_values = [h3.get_text(strip=True) for h3 in h3_elements]
+
+        url = self.list_of_urls[1]
         titles = []
-        a_elements = soup.select('.posting-list-item')
-        href_values = [element['href'] for element in a_elements]
-        base_url = "https://nofluffjobs.com" 
-                
-        for x in range(len(text_values)):
-            if x < len(href_values):
-                titles.append({'title': text_values[x], "link":urljoin(base_url, href_values[x])})
-        self.main_dict['nofluffjobs'] = titles
-        
+
+        while True:
+
+            response = requests.get(url)
+            soup = BeautifulSoup(response.text, "html.parser")
+            h3_elements = soup.select('nfj-postings-list h3')
+            text_values = [h3.get_text(strip=True) for h3 in h3_elements]
+            
+            a_elements = soup.select('.posting-list-item')
+            href_values = [element['href'] for element in a_elements]
+            base_url = "https://nofluffjobs.com" 
+                    
+            for x in range(len(text_values)):
+                try:
+                    if x < len(href_values):
+                        titles.append({'title': text_values[x], "link":(base_url + href_values[x])})
+                except AttributeError:
+                    None
+
+            pagination = soup.select('a[aria-label="Next"]')
+            if pagination:
+                url = base_url + pagination[0]['href']
+            else:
+                break
+
+        self.main_dict['nofluffjobs'] = titles                
         
     def pracuj(self):
-        response = requests.get(self.list_of_urls[0])
-        soup = BeautifulSoup(response.text, "html.parser")
+        
         titles = []
-        list_of_headings = soup.find_all("h2", class_="listing_buap3b6")
-        for x in range(len(list_of_headings)):
-            try:
-                heading = list_of_headings[x].find("a")
-                titles.append({'title': heading.text, "link":heading["href"]})
-            except AttributeError:
-                None
+        parent_url = self.list_of_urls[0]
+
+        while True:
+            
+            response = requests.get(self.list_of_urls[0])
+            soup = BeautifulSoup(response.text, "html.parser")
+            list_of_headings = soup.find_all("h2", class_="listing_buap3b6")
+            
+            difference = len(parent_url)-len(self.list_of_urls[0])
+            
+            for x in range(len(list_of_headings)):
+                try:
+                    heading = list_of_headings[x].find("a")
+                    titles.append({'title': heading.text, "link":heading["href"]})
+                except AttributeError:
+                    None
+
+            if soup.find(class_="listing_n5kktfs size-small variant-secondary listing_b1fqykql"):   
+                arr = list(self.list_of_urls[0])
+                digit = int(''.join(arr[(difference-1):]))
+                digit+=1
+                arr[(difference-1):] = str(digit)
+                self.list_of_urls[0] = ''.join(arr)
+            else:
+                break
             
         self.main_dict['pracuj'] = titles
-        
 
 
     def json(self):
